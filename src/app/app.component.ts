f2f4fff0aa442df09b833aa110babd9ccb217f8b
ScrimReaper <166304133+ScrimReaper@users.noreply.github.com>
2025-07-11 20:40:01 +0200

    build(dependencies):added bootstrap and jwt-decode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {HeaderComponent} from "./component/header/header.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, NgbModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
