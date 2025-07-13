2a6a828158b914451d444df329f7dc8060f3052e
ScrimReaper <166304133+ScrimReaper@users.noreply.github.com>
2025-07-13 21:09:38 +0200

    feat(routes): add routing for dashboard and login with AuthGuard protection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HeaderComponent} from "./component/header/header.component";
 import {CommonModule} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgbModule, HeaderComponent],
+  imports: [CommonModule, NgbModule, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
