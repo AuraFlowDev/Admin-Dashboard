3b783305894c71b6debe16e24bb1689fb77b519a
ScrimReaper <166304133+ScrimReaper@users.noreply.github.com>
2025-07-25 19:07:17 +0200

    feat(error): add handleError function to improve error handling

--- src/app/dto/ErrorDto.ts
+++ src/app/dto/ErrorDto.ts
@@ -1,3 +1,11 @@
 export class ErrorDto {
   error: string;
 }
+
+
+export function handleError(error: any): string {
+  if (error.error.error) {
+    return error.error.error;
+  }
+  return 'An unexpected error occurred.';
+}
