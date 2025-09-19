e681f2a6c2442fb4df3cf40276cdcf95cbe8caff
ScrimReaper <166304133+ScrimReaper@users.noreply.github.com>
2025-09-19 13:55:23 +0200

    feat(formatter): add role formatting method to display user roles

--- src/app/utils/Formatter.ts
+++ src/app/utils/Formatter.ts
@@ -5,4 +5,14 @@ export class Formatter {
     }
     return new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format(amount)
   }
+
+
+  public static formatRoles(roles: string[] | undefined): string {
+    if (!roles || roles.length === 0) {
+      return 'No roles';
+
+    }
+    const formattedroles = roles.map(role => role.replace('ROLE_', '')).map(role => role.charAt(0).toUpperCase() + role.slice(1).toLowerCase())
+    return formattedroles.join(', ');
+  }
 }
