b6990c49c6457ea3ea9151c92d064e4201b85251
ScrimReaper <166304133+ScrimReaper@users.noreply.github.com>
2025-09-29 19:17:50 +0200

    feat(packages): implement packages UI with dynamic listing and service integration
    - Implemented `PackagesComponent` to display a dynamic list of packages retrieved from `PackageService`
    - Added "Create Package" button as placeholder for future modal implementation

--- src/app/component/packages/packages.component.ts
+++ src/app/component/packages/packages.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PackageDto} from "../../dto/PackageDtos";
+import {PackageService} from "../../services/packages.service";
+import {ToastrService} from "ngx-toastr";
+import {NgForOf} from "@angular/common";
+import {Formatter} from "../../utils/Formatter";
 
 @Component({
   selector: 'app-packages',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './packages.component.html',
   styleUrl: './packages.component.scss'
 })
-export class PackagesComponent {
+export class PackagesComponent implements OnInit {
 
+  packages: PackageDto[] = [];
+
+  ngOnInit(): void {
+    this.loadPackages();
+  }
+
+  constructor(private service: PackageService, private toastr: ToastrService) {
+  }
+
+  loadPackages() {
+    this.service.getPackages().subscribe({
+      next: (data) => {
+        this.packages = data.packages;
+      },
+      error: (err) => {
+        this.toastr.error(err.error.error);
+      }
+    })
+  }
+
+
+  protected readonly Formatter = Formatter;
 }
