c5105694df8530dea17d292808dbe979076c739c
ScrimReaper <166304133+scrimreaper@users.noreply.github.com>
2025-09-30 15:16:54 +0200

    feat(packages): integrate create package functionality with modal component
    - Added `createPackage`, `openCreateModal`, and `closeCreateModal` methods in `PackagesComponent`
    - Implemented modal interaction for package creation using `PackageModalComponent`
    - Enhanced template to bind modal visibility and handle confirmation and cancellation actions

--- src/app/component/packages/packages.component.ts
+++ src/app/component/packages/packages.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from '@angular/core';
-import {PackageDto} from "../../dto/PackageDtos";
+import {PackageCreateDto, PackageDto} from "../../dto/PackageDtos";
 import {PackageService} from "../../services/packages.service";
 import {ToastrService} from "ngx-toastr";
 import {NgForOf} from "@angular/common";
 import {Formatter} from "../../utils/Formatter";
+import {PackageModalComponent} from "./packagemodal/packagemodal.component";
+import {ConfirmDialogComponent} from "../shared/confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: 'app-packages',
   standalone: true,
   imports: [
     NgForOf
+    NgForOf,
+    PackageModalComponent,
   ],
   templateUrl: './packages.component.html',
   styleUrl: './packages.component.scss'
@@ -17,6 +21,7 @@ import {Formatter} from "../../utils/Formatter";
 export class PackagesComponent implements OnInit {
 
   packages: PackageDto[] = [];
+  showCreateModal: boolean = false;
 
   ngOnInit(): void {
     this.loadPackages();
@@ -34,6 +39,28 @@ export class PackagesComponent implements OnInit {
         this.toastr.error(err.error.error);
       }
     })
+  }
+
+  createPackage(dto: PackageCreateDto) {
+    this.service.createPackage(dto).subscribe({
+      next: (data) => {
+        this.toastr.success("Package created");
+        this.packages.push(data);
+      },
+      error: (err) => {
+        this.toastr.error(err.error.error);
+      }
+    })
+    this.showCreateModal = false;
+  }
+
+  closeCreateModal() {
+    this.showCreateModal = false;
+  }
+
+  openCreateModal() {
+    this.showCreateModal = true;
+    console.log("open modal:", this.showCreateModal);
   }
 
 
