67b564a00d8031fe0132fdc11c0f178572e25b61
ScrimReaper <166304133+scrimreaper@users.noreply.github.com>
2025-09-30 15:07:59 +0200

    fix(globals): update backend URL to secure dev environment

--- src/app/globals/globals.ts
+++ src/app/globals/globals.ts
@@ -4,7 +4,7 @@ import {Injectable} from "@angular/core";
   providedIn: 'root'
 })
 export class Globals {
-  private static readonly backendUrl: string = 'http://194.182.175.77/api';
+  private static readonly backendUrl: string = 'https://dev.auraflow.at/api';
   static readonly defaultPageSize: number = 10;
 
   get backendUrl(): string {
