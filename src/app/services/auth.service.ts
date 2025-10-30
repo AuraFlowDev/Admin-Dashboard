13886a705d275e28d167847e2b751568ff0f5fce
ScrimReaper <166304133+scrimreaper@users.noreply.github.com>
2025-10-30 18:58:23 +0100

    feat(auth): support one-factor and two-factor login flows
    - Updated `AuthService` to handle both OneFactorResponse and TwoFactorResponse authentication types
    - Added `roles` and `hasRole` methods for role-based access control
    - Introduced `handle1FA` and `handle2FA` methods in `LoginComponent` to manage different login workflows
    - Registered `/verify/:reqId` route for two-factor authentication with the `TwoFactorComponent`

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,12 @@
 import {Injectable} from '@angular/core';
-import {AuthResponseDto, LoginDto} from "../dto/LoginDto";
+import {isOneFactorResponse, LoginDto, LoginResponse, OneFactorResponse, VerifyRequest} from "../dto/LoginDto";
 import {Observable, tap} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Globals} from "../globals/globals";
 import {jwtDecode} from "jwt-decode";
+import {Role} from "../dto/PrivilegeDtos";
+import {setParamsFromDto} from "../utils/SetParamsFromDto";
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +19,12 @@ export class AuthService {
   constructor(private http: HttpClient, private globals: Globals) {
   }
 
-  loginUser(loginDto: LoginDto): Observable<AuthResponseDto> {
-    return this.http.post<AuthResponseDto>(this.authUrl + "/login", loginDto).pipe(
-      tap(dto => {
-        this.setToken(dto.token);
+  loginUser(loginDto: LoginDto): Observable<LoginResponse> {
+    return this.http.post<any>(this.authUrl + "/login", loginDto).pipe(
+      tap((dto) => {
+        if (isOneFactorResponse(dto)) {
+          this.setToken(dto.token);
+        }
       })
     )
   }
@@ -40,6 +45,18 @@ export class AuthService {
   isLoggedIn(): boolean {
     return !!this.token && (this.getTokenExpirationDate(this.token).valueOf() > new Date().valueOf())
   }
+
+  get roles(): Role[] {
+    const decoded = jwtDecode<{ roles: string[] }>(this.token!);
+
+    return (decoded.roles ?? [])
+      .filter((r): r is Role => Object.values(Role).includes(r as Role));
+  }
+
+  hasRole(role: Role): boolean {
+    return this.roles.includes(role);
+  }
+
 
   private getTokenExpirationDate(token: string): Date {
 
