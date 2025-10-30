ee73bc5c8d1c4225aa6515109a62682d6e92781a
ScrimReaper <166304133+scrimreaper@users.noreply.github.com>
2025-10-30 20:15:18 +0100

    feat(two-factor): implement two-factor verification functionality
    - Added `verifyTwoFactor` method to `AuthService` for handling two-factor authentication requests
    - Implemented form validation and submission logic in `TwoFactorComponent` for verifying codes
    - Updated template with a responsive UI for two-factor code input and error handling
    - Integrated navigation and toastr notifications for a better user experience

--- src/app/component/two-factor/two-factor.component.ts
+++ src/app/component/two-factor/two-factor.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from "../../services/auth.service";
+import {VerifyRequest} from "../../dto/LoginDto";
+import {NgIf} from "@angular/common";
+import {ToastrService} from "ngx-toastr";
+import {handleError} from "../../dto/ErrorDto";
 
 @Component({
   selector: 'app-two-factor',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './two-factor.component.html',
   styleUrl: './two-factor.component.scss'
 })
-export class TwoFactorComponent {
+export class TwoFactorComponent implements OnInit {
+  reqId!: string;
+  form!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: AuthService,
+    private toastr: ToastrService) {
+  }
+
+  ngOnInit(): void {
+    this.reqId = this.route.snapshot.paramMap.get('reqId') ?? '';
+    this.form = this.fb.group({
+      code: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const dto: VerifyRequest = {
+      reqId: this.reqId,
+      token: this.form.value.code as string
+    };
+
+    this.service.verifyTwoFactor(dto).subscribe({
+      next: (res) => {
+        this.toastr.success("Welcome Back " + res.user.firstname);
+        const redirectTo = this.route.snapshot.queryParamMap.get('redirectTo') || '/';
+        this.router.navigateByUrl(redirectTo);
+      },
+      error: (err) => {
+        this.toastr.error(handleError(err))
+      }
+    })
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/login']);
+  }
 }
